a66b145422e2345ffce2b294249be31087e19919
Your Name <you@example.com>
2022-06-29 09:41:56 +0200

    Initial commit

---
+++ src/main/java/de/mischok/attendancemanager/AttendancemanagerApplication.java
@@ -0,0 +1,13 @@
+package de.mischok.attendancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendancemanagerApplication.class, args);
+	}
+
+}
